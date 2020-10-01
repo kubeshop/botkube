1cb3604b7980a12c0e2adf3208eb568482c3bb9d
Akanksha kumari <41359156+A-kanksh-a@users.noreply.github.com>
2020-10-01 10:36:32 +0530

    Update test/e2e/utils/utils.go removed line
    Co-authored-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- test/e2e/utils/utils.go
+++ test/e2e/utils/utils.go
@@ -23,7 +23,6 @@ import (
 	"testing"
 
 	"k8s.io/apimachinery/pkg/types"
-
 	"github.com/nlopes/slack"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
