746a6b463be29ed210b9610ca88de2fda4d3f343
Abhishek <shabhish@cisco.com>
2020-10-02 14:27:24 +0530

    fix indentation

--- hack/kind-cluster.sh
+++ hack/kind-cluster.sh
@@ -21,38 +21,38 @@
 set -e
 
 install_kind() {
-    echo "Installing KIND cluster"
-    GO111MODULE="on" go get sigs.k8s.io/kind@v0.9.0
+  echo "Installing KIND cluster"
+  GO111MODULE="on" go get sigs.k8s.io/kind@v0.9.0
 }
 
 create_kind_cluster() {
-    install_kind
-    echo "creating KIND cluster"
-    kind create cluster
+  install_kind
+  echo "creating KIND cluster"
+  kind create cluster
 }
 
 destroy_kind_cluster() {
-    echo "destroying KIND cluster"
-    kind delete clusters --all
+  echo "destroying KIND cluster"
+  kind delete clusters --all
 }
 
 help() {
-    usage="$(basename "$0") [option] -- Script to create or destroy KIND cluster.
-    Available option are install-kind, destroy-kind or create-kind"
-    echo $usage
+  usage="$(basename "$0") [option] -- Script to create or destroy KIND cluster.
+  Available option are install-kind, destroy-kind or create-kind"
+  echo $usage
 }
 
 
 if [ $# -gt 1 ]; then help ;fi
 case "${1}" in
-    create-kind)
-        create_kind_cluster
-        ;;
-    destroy-kind)
-        destroy_kind_cluster
-        ;;
-    *)
-        help
-        exit 1
+  create-kind)
+    create_kind_cluster
+    ;;
+  destroy-kind)
+    destroy_kind_cluster
+    ;;
+  *)
+    help
+    exit 1
 esac
 
