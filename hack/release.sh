e58ba290a1730900532d4e75d6000e47ad97354d
Pawel Kosiec <pawel.kosiec@gmail.com>
2022-06-30 16:38:57 +0200

    Increase timeout and uncomment lines

--- hack/release.sh
+++ hack/release.sh
@@ -61,8 +61,8 @@ update_chart_yamls() {
 fetch_previous_version
 update_chart_yamls $previous_version $version
 update_chart_yamls "v9.99.9-dev" $version
-#generate_changelog $version
-#make release
+generate_changelog $version
+make release
 
 echo "=========================== Done ============================="
 echo "Congratulations!! Release ${version} published."
