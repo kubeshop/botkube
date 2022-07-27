f9c6467251f13e93739715db8e79e0dd89c4544c
Paweł Kosiec <pawel.kosiec@gmail.com>
2022-07-27 13:49:50 +0200

    Refactor ShortMessage format function

--- pkg/format/short_msg.go
+++ pkg/format/short_msg.go
@@ -2,114 +2,91 @@ package format
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/kubeshop/botkube/pkg/config"
 	"github.com/kubeshop/botkube/pkg/events"
 )
 
+const bulletPointFmt = "- %s\n"
+
 // ShortMessage prepares message in short event format.
-func ShortMessage(event events.Event) (msg string) {
-	additionalMsg := ""
-	if len(event.Messages) > 0 {
-		for _, m := range event.Messages {
-			additionalMsg += fmt.Sprintf("%s\n", m)
-		}
-	}
-	if len(event.Recommendations) > 0 {
-		recommend := ""
-		for _, m := range event.Recommendations {
-			recommend += fmt.Sprintf("- %s\n", m)
-		}
-		additionalMsg += fmt.Sprintf("Recommendations:\n%s", recommend)
-	}
-	if len(event.Warnings) > 0 {
-		warning := ""
-		for _, m := range event.Warnings {
-			warning += fmt.Sprintf("- %s\n", m)
-		}
-		additionalMsg += fmt.Sprintf("Warnings:\n%s", warning)
+func ShortMessage(event events.Event) string {
+	msg := messageHeader(event)
+	msgAttachments := messageAttachments(event)
+
+	return fmt.Sprintf("%s\n%s", msg, msgAttachments)
+}
+
+func messageHeader(event events.Event) string {
+	resourceName := event.Name
+	if event.Namespace != "" {
+		resourceName = fmt.Sprintf("%s/%s", event.Namespace, event.Name)
 	}
 
 	switch event.Type {
 	case config.CreateEvent, config.DeleteEvent, config.UpdateEvent:
-		switch event.Kind {
-		case "Namespace", "Node", "PersistentVolume", "ClusterRole", "ClusterRoleBinding":
-			msg = fmt.Sprintf(
-				"%s *%s* has been %s in *%s* cluster\n",
-				event.Kind,
-				event.Name,
-				event.Type+"d",
-				event.Cluster,
-			)
-		default:
-			msg = fmt.Sprintf(
-				"%s *%s/%s* has been %s in *%s* cluster\n",
-				event.Kind,
-				event.Namespace,
-				event.Name,
-				event.Type+"d",
-				event.Cluster,
-			)
-		}
+		return fmt.Sprintf(
+			"%s *%s* has been %s in *%s* cluster",
+			event.Kind,
+			resourceName,
+			event.Type+"d",
+			event.Cluster,
+		)
+
 	case config.ErrorEvent:
-		switch event.Kind {
-		case "Namespace", "Node", "PersistentVolume", "ClusterRole", "ClusterRoleBinding":
-			msg = fmt.Sprintf(
-				"Error Occurred in %s: *%s* in *%s* cluster\n",
-				event.Kind,
-				event.Name,
-				event.Cluster,
-			)
-		default:
-			msg = fmt.Sprintf(
-				"Error Occurred in %s: *%s/%s* in *%s* cluster\n",
-				event.Kind,
-				event.Namespace,
-				event.Name,
-				event.Cluster,
-			)
-		}
+		return fmt.Sprintf(
+			"Error Occurred in %s: *%s* in *%s* cluster",
+			event.Kind,
+			resourceName,
+			event.Cluster,
+		)
+
 	case config.WarningEvent:
-		switch event.Kind {
-		case "Namespace", "Node", "PersistentVolume", "ClusterRole", "ClusterRoleBinding":
-			msg = fmt.Sprintf(
-				"Warning %s: *%s* in *%s* cluster\n",
-				event.Kind,
-				event.Name,
-				event.Cluster,
-			)
-		default:
-			msg = fmt.Sprintf(
-				"Warning %s: *%s/%s* in *%s* cluster\n",
-				event.Kind,
-				event.Namespace,
-				event.Name,
-				event.Cluster,
-			)
-		}
+		return fmt.Sprintf(
+			"Warning %s: *%s* in *%s* cluster",
+			event.Kind,
+			resourceName,
+			event.Cluster,
+		)
+
 	case config.InfoEvent, config.NormalEvent:
-		switch event.Kind {
-		case "Namespace", "Node", "PersistentVolume", "ClusterRole", "ClusterRoleBinding":
-			msg = fmt.Sprintf(
-				"%s Info: *%s* in *%s* cluster\n",
-				event.Kind,
-				event.Name,
-				event.Cluster,
-			)
-		default:
-			msg = fmt.Sprintf(
-				"%s Info: *%s/%s* in *%s* cluster\n",
-				event.Kind,
-				event.Namespace,
-				event.Name,
-				event.Cluster,
-			)
+		return fmt.Sprintf(
+			"%s Info: *%s* in *%s* cluster",
+			event.Kind,
+			resourceName,
+			event.Cluster,
+		)
+	}
+
+	return ""
+}
+
+func messageAttachments(event events.Event) string {
+	var additionalMsgStrBuilder strings.Builder
+	if len(event.Messages) > 0 {
+		for _, m := range event.Messages {
+			additionalMsgStrBuilder.WriteString(fmt.Sprintf("%s\n", m))
+		}
+	}
+	if len(event.Recommendations) > 0 {
+		additionalMsgStrBuilder.WriteString("Recommendations:\n")
+
+		for _, m := range event.Recommendations {
+			additionalMsgStrBuilder.WriteString(fmt.Sprintf(bulletPointFmt, m))
+		}
+	}
+	if len(event.Warnings) > 0 {
+		additionalMsgStrBuilder.WriteString("Warnings:\n")
+
+		for _, m := range event.Warnings {
+			additionalMsgStrBuilder.WriteString(fmt.Sprintf(bulletPointFmt, m))
 		}
 	}
 
-	// Add message in the attachment if there is any
-	if len(additionalMsg) > 0 {
-		msg += fmt.Sprintf("```\n%s```", additionalMsg)
+	if additionalMsgStrBuilder.Len() == 0 {
+		return ""
 	}
-	return msg
+
+	return fmt.Sprintf("```\n%s```", additionalMsgStrBuilder.String())
 }
