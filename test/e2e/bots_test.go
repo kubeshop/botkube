11e4b9d943b51da73f3a3fe89269d7446f469763
Josef Karasek <josef@kubeshop.io>
2023-01-31 10:42:39 +0100

    fix help msg

--- test/e2e/bots_test.go
+++ test/e2e/bots_test.go
@@ -329,24 +329,24 @@ func runBotTest(t *testing.T,
 				The official Botkube plugin for the Helm CLI.
 
 				Usage:
-				helm [command]
+				  helm [command]
 				
 				Available Commands:
-				install     # Installs a given chart to cluster where Botkube is installed.
-				list        # Lists all releases on cluster where Botkube is installed.
-				rollback    # Rolls back a given release to a previous revision.
-				status      # Displays the status of the named release.
-				test        # Runs tests for a given release.
-				uninstall   # Uninstalls a given release.
-				upgrade     # Upgrades a given release.
-				version     # Shows the version of the Helm CLI used by this Botkube plugin.
-				history     # Shows release history
-				get         # Shows extended information of a named release
+				  install     # Installs a given chart to cluster where Botkube is installed.
+				  list        # Lists all releases on cluster where Botkube is installed.
+				  rollback    # Rolls back a given release to a previous revision.
+				  status      # Displays the status of the named release.
+				  test        # Runs tests for a given release.
+				  uninstall   # Uninstalls a given release.
+				  upgrade     # Upgrades a given release.
+				  version     # Shows the version of the Helm CLI used by this Botkube plugin.
+				  history     # Shows release history
+				  get         # Shows extended information of a named release
 				
 				Flags:
-					--namespace,-n
-					--debug
-					--burst-limit
+				    --namespace,-n
+				    --debug
+				    --burst-limit
 				
 				Use "helm [command] --help" for more information about the command.`))
 
