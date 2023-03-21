f85e7222e0244534ddd21fdc1bea5df6ab9e33fb
Josef Karasek <josef@kubeshop.io>
2023-03-21 11:28:18 +0100

    implement fn

--- pkg/pluginx/kubeconfig.go
+++ pkg/pluginx/kubeconfig.go
@@ -1,5 +1,34 @@
 package pluginx
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
-func PersistKubeConfig(ctx context.Context, kc []byte) (string, func(context.Context) error, error)
+func PersistKubeConfig(ctx context.Context, kc []byte) (string, func(context.Context) error, error) {
+	if len(kc) == 0 {
+		return "", nil, fmt.Errorf("received empty kube config")
+	}
+
+	file, err := os.CreateTemp("", "kubeconfig-")
+	if err != nil {
+		return "", nil, err
+	}
+
+	abs, err := filepath.Abs(file.Name())
+	if err != nil {
+		return "", nil, err
+	}
+
+	if _, err = file.Write(kc); err != nil {
+		return "", nil, err
+	}
+
+	deleteFn := func(context.Context) error {
+		return os.Remove(abs)
+	}
+
+	return abs, deleteFn, nil
+}
