a37faccaec8f642df77a09f09c43e177b8f3cfa6
Josef Karasek <josef@kubeshop.io>
2023-05-17 17:15:33 +0200

    Hackathon migration cli

---
+++ botkube-cloud-backend/cmd/cli/cmd/config/config.go
@@ -0,0 +1,71 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/kubeshop/botkube-cloud/botkube-cloud-backend/internal/cli"
+	"github.com/kubeshop/botkube-cloud/botkube-cloud-backend/internal/cli/heredoc"
+	"k8s.io/client-go/util/homedir"
+)
+
+var (
+	configFilePath = filepath.Join(homedir.HomeDir(), ".botkube", "cloud.json")
+	loginCmd       = heredoc.WithCLIName(`login with: <cli> login`, cli.Name)
+)
+
+// Config is botkuce cli config
+type Config struct {
+	Token string `json:"token"`
+}
+
+// New creates new Config from local data
+func New() (*Config, error) {
+	c := &Config{}
+	err := c.Read()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config: %v\n%s", err, loginCmd)
+	}
+	return c, nil
+}
+
+// Save saves Config to local FS
+func (c *Config) Save() error {
+	if _, err := os.Stat(filepath.Dir(configFilePath)); os.IsNotExist(err) {
+		// #nosec G301
+		err := os.MkdirAll(filepath.Dir(configFilePath), 0755)
+		if err != nil {
+			return fmt.Errorf("failed to create config directory: %v", err)
+		}
+	}
+
+	data, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal configuration: %v", err)
+	}
+
+	// #nosec G306
+	err = os.WriteFile(configFilePath, data, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write config: %v", err)
+	}
+
+	return nil
+}
+
+// Read reads Config from local FS
+func (c *Config) Read() error {
+	data, err := os.ReadFile(filepath.Clean(configFilePath))
+	if err != nil {
+		return fmt.Errorf("failed to read config file: %v", err)
+	}
+
+	err = json.Unmarshal(data, c)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal config file: %v", err)
+	}
+
+	return nil
+}
