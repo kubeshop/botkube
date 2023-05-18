2c59adb69b0b0864e1ca79bbbf94120c39521daf
Josef Karasek <josef@kubeshop.io>
2023-05-18 16:23:01 +0200

    Hackathon migration cli

---
+++ botkube-cloud-backend/cmd/migration/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+
+	"github.com/kubeshop/botkube-cloud/botkube-cloud-backend/cmd/migration/internal/config"
+	"gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/retry"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+func main() {
+	files, _, err := config.NewEnvProvider().Configs(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	conf, err := config.LoadWithDefaults(files)
+	if err != nil {
+		panic(err)
+	}
+	yamlBytes, err := yaml.Marshal(conf)
+	if err != nil {
+		panic(err)
+	}
+	if err := createOrUpdateCM(context.Background(), yamlBytes); err != nil {
+		panic(err)
+	}
+
+}
+
+func createOrUpdateCM(ctx context.Context, config []byte) error {
+	k8sClient, err := newK8sClient()
+	if err != nil {
+		return err
+	}
+	cm := newCM()
+	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		_, corErr := ctrlutil.CreateOrUpdate(ctx, k8sClient, cm, func() error {
+			cm.BinaryData = map[string][]byte{
+				"config.yaml": config,
+			}
+			return nil
+		})
+		return corErr
+	})
+}
+
+func newK8sClient() (client.Client, error) {
+	k8sConfig, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		return nil, err
+	}
+	return client.New(k8sConfig, client.Options{})
+}
+
+func newCM() *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "botkube-migration",
+			Namespace: "botkube",
+			Labels: map[string]string{
+				"app": "botkube-migration",
+			},
+		},
+	}
+}
