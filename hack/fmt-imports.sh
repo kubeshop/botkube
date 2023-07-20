988389d69b7c3e2b8d8f1dfe2233fe53c95765ae
Pawel Kosiec <pawel.kosiec@gmail.com>
2023-07-20 11:03:11 +0200

    Lint migration test (#1159)
    * Lint migration test
    
    * Revise tests when it comes to imports

--- hack/fmt-imports.sh
+++ hack/fmt-imports.sh
@@ -9,7 +9,7 @@ set -E         # needs to be set if we want the ERR trap
 
 CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 REPO_ROOT_DIR=$(cd "${CURRENT_DIR}/.." && pwd)
-GOIMPORTS_REVISER_VERSION=3.3.0
+GOIMPORTS_REVISER_VERSION=3.3.1
 
 host::os() {
   local host_os
@@ -86,9 +86,14 @@ imports::format() {
   echo "Executing goimports-reviser..."
   pushd "$REPO_ROOT_DIR" > /dev/null
 
-  goimports-reviser -rm-unused -project-name github.com/kubeshop/botkube -recursive ./pkg
+  echo "- Revising cmd..."
   goimports-reviser -rm-unused -project-name github.com/kubeshop/botkube -recursive ./cmd
+  echo "- Revising internal..."
   goimports-reviser -rm-unused -project-name github.com/kubeshop/botkube -recursive ./internal
+  echo "- Revising pkg..."
+  goimports-reviser -rm-unused -project-name github.com/kubeshop/botkube -recursive ./pkg
+  echo "- Revising test..."
+  goimports-reviser -rm-unused -project-name github.com/kubeshop/botkube -recursive ./test
 
   popd > /dev/null
 }
