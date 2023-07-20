fbf8aa08a9ea58a9e1cf8bf578b8847c4549cc2a
Huseyin BABAL <huseyinbabal88@gmail.com>
2023-07-20 15:03:14 +0300

    Keptn plugin added

---
+++ cmd/source/keptn/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/hashicorp/go-plugin"
+
+	"github.com/kubeshop/botkube/internal/source/keptn"
+	"github.com/kubeshop/botkube/pkg/api/source"
+)
+
+// version is set via ldflags by GoReleaser.
+var version = "dev"
+
+func main() {
+	source.Serve(map[string]plugin.Plugin{
+		keptn.PluginName: &source.Plugin{
+			Source: keptn.NewSource(version),
+		},
+	})
+}
