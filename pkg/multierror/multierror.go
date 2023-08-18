f96d05f7894a2c028620c9894a64d17a39f97c7e
SubhajitDutta07 <sencer.subhajitdutta@gmail.com>
2023-08-18 22:40:21 +0530

    Modified fomatting of errs

--- pkg/multierror/multierror.go
+++ pkg/multierror/multierror.go
@@ -29,15 +29,6 @@ func listFormatFunc(es []error) string {
 		return fmt.Sprintf("1 error occurred:\n\t* %s", es[0])
 	}
 
-	// points := make([]string, len(es))
-	// for i, err := range es {
-	// 	points[i] = fmt.Sprintf("* %s", err)
-	// }
-
-	// return fmt.Sprintf(
-	// 	"%d errors occurred:\n\t%s",
-	// 	len(es), strings.Join(points, "\n\t"))
-
 	formatString := "* %s"
 
 	formatterResult := strings.Join(formatElements(formatString,es),"\n\t")
