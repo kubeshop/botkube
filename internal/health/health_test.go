f3fb89a481a595c68311a70c276c14ff9d79d6bc
Tomek Rogalski <tomasz@madebyrogal.com>
2023-10-03 15:27:50 +0200

    add tests

---
+++ internal/health/health_test.go
@@ -0,0 +1,63 @@
+package health
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/kubeshop/botkube/pkg/config"
+)
+
+func TestServeHTTPUnavailable(t *testing.T) {
+	// given
+	checker := NewChecker(context.TODO(), &config.Config{}, nil)
+	expectedStatus := checker.getStatus()
+
+	req, err := http.NewRequest("GET", "/", nil)
+	require.NoError(t, err)
+	rr := httptest.NewRecorder()
+
+	// when
+	checker.ServeHTTP(rr, req)
+
+	// then
+	assert.Equal(t, http.StatusServiceUnavailable, rr.Code)
+	assert.Equal(t, "application/json", rr.Header().Get("Content-Type"))
+
+	var resp status
+	err = json.Unmarshal(rr.Body.Bytes(), &resp)
+	require.NoError(t, err)
+
+	assert.Equal(t, BotkubeStatusUnhealthy, resp.Botkube.Status)
+	assert.Equal(t, resp.Botkube.Status, expectedStatus.Botkube.Status)
+}
+
+func TestServeHTTPOK(t *testing.T) {
+	// given
+	checker := NewChecker(context.TODO(), &config.Config{}, nil)
+	checker.MarkAsReady()
+	expectedStatus := checker.getStatus()
+
+	req, err := http.NewRequest("GET", "/", nil)
+	require.NoError(t, err)
+	rr := httptest.NewRecorder()
+
+	// when
+	checker.ServeHTTP(rr, req)
+
+	// then
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Equal(t, "application/json", rr.Header().Get("Content-Type"))
+
+	var resp status
+	err = json.Unmarshal(rr.Body.Bytes(), &resp)
+	require.NoError(t, err)
+
+	assert.Equal(t, BotkubeStatusHealthy, resp.Botkube.Status)
+	assert.Equal(t, resp.Botkube.Status, expectedStatus.Botkube.Status)
+}
